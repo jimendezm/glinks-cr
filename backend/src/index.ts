3b7dc633b83ea29f788b3c3b355fb900b66f3bed
Ricardo Arce <yasserarce2404@gmail.com>
2026-04-06 22:24:47 -0600

    feat: catalog tables routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,6 @@ import { prisma } from './lib/prisma';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DATABASE_URL = process.env.DATABASE_URL;
 
 // Note: When we deploy this to production, we need to specify in cors the allowed origins.
 // Currently, any origin is allowed, which is somewhat insecure.
@@ -14,8 +13,28 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
-})
+});
+
+app.get('/plans', async (req, res) => {
+  const data = await prisma.planCatalog.findMany();
+  res.json({ data });
+});
+
+app.get('/sectors', async (req, res) => {
+  const data = await prisma.sectorCatalog.findMany();
+  res.json({ data });
+});
+
+app.get('/ap-types', async (req, res) => {
+  const data = await prisma.apTypeCatalog.findMany();
+  res.json({ data });
+});
+
+app.get('/roles', async (req, res) => {
+  const data = await prisma.roleCatalog.findMany();
+  res.json({ data });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-})
+});
